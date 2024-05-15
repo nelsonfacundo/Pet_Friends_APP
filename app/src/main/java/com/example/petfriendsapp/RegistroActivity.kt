7114123c9c310ebbfc88eb0d1f5580fc98808267
[Mayra] <[mayrayaz03@gmail.com]>
2024-05-15 19:31:23 -0300

    Navegacion login-registro y viceversa

--- app/src/main/java/com/example/petfriendsapp/RegistroActivity.kt
+++ app/src/main/java/com/example/petfriendsapp/RegistroActivity.kt
@@ -2,31 +2,35 @@ package com.example.petfriendsapp
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class LoginActivity : AppCompatActivity() {
-    lateinit var txt_registro: TextView
+class RegistroActivity : AppCompatActivity() {
+    private lateinit var backButton: ImageView
+    private lateinit var bntRegistrar: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.login_activity)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.login)) { v, insets ->
+        setContentView(R.layout.activity_registro)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.activity_registrar)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        txt_registro = findViewById(R.id.registro)
+        backButton = findViewById(R.id.ic_back)
+        bntRegistrar = findViewById(R.id.btnRegistrar)
 
-        txt_registro.setOnClickListener {
-            val intent = Intent(this, RegistroActivity::class.java)
+        backButton.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
+
         }
     }
-
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,8 @@ buildscript {
         mavenCentral()
     }
     dependencies {
+        val nav_version = "2.7.7"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version");
         classpath("com.android.tools.build:gradle:7.1.0")
     }
 }
