55e0246da742838615b4d539bc157fba358bddfe
Francisco Ramponi <ramponifran@gmail.com>
2024-05-15 22:36:32 -0300

    Navgraph, logica del menu, agregado de NavigationUI

--- app/src/main/java/com/example/petfriendsapp/LoginActivity.kt
+++ app/src/main/java/com/example/petfriendsapp/LoginActivity.kt
@@ -1,6 +1,10 @@
 package com.example.petfriendsapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +19,21 @@ class LoginActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val loginButton: Button = findViewById(R.id.btn_login)
+        loginButton.setOnClickListener {
+            val nombreUsuario = findViewById<EditText>(R.id.input_user)
+            val contraseña: EditText = findViewById(R.id.input_pass)
+            var credencialesValidas: Boolean = true
+
+            if (credencialesValidas) {
+                // Si las credenciales son válidas, navega a la siguiente actividad
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            } else {
+                // Si las credenciales son inválidas, muestra un mensaje de error
+                Toast.makeText(this, "Credenciales inválidas", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/com/example/petfriendsapp/MainActivity.kt
+++ app/src/main/java/com/example/petfriendsapp/MainActivity.kt
@@ -5,8 +5,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomNavView: BottomNavigationView
+    private lateinit var navHostFragment: NavHostFragment
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +21,10 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+
         }
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        bottomNavView = findViewById(R.id.bottom_var)
+        NavigationUI.setupWithNavController(bottomNavView,navHostFragment.navController)
     }
 }
