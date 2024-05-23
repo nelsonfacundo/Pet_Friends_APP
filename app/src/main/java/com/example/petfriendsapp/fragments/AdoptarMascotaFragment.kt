ead38cf69077c429fb129a8a9ee30b7104ac8b21
maticeri1 <maticeri1@gmail.com>
2024-05-23 18:28:00 -0300

    Creacion fragmentos detalleMascota y AdoptarMascota

---
+++ app/src/main/java/com/example/petfriendsapp/fragments/AdoptarMascotaFragment.kt
@@ -0,0 +1,30 @@
+package com.example.petfriendsapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.petfriendsapp.R
+
+
+class AdoptarMascotaFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_adoptar_mascota, container, false)
+    }
+
+    companion object {
+
+    }
+}
