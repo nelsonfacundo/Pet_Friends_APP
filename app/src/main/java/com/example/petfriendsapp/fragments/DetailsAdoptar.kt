7a34e5b4916bcae08982fc10b0718cb1f4bb11fe
Matias Quesada <matiasquesada00@gmail.com>
2024-05-24 22:01:23 -0300

    navegacion de recycler a detalle de Mascota

--- app/src/main/java/com/example/petfriendsapp/fragments/DetailsAdoptar.kt
+++ app/src/main/java/com/example/petfriendsapp/fragments/DetailsAdoptar.kt
@@ -1,60 +1,43 @@
 package com.example.petfriendsapp.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.example.petfriendsapp.R
+import com.example.petfriendsapp.entities.Mascota
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailsAdoptar.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DetailsAdoptar : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
+   private val args: DetailsAdoptarArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details_adoptar, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_details_adoptar, container, false)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailsAdoptar.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetailsAdoptar().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+
+
+        // Bind the details to the views
+        val txtRaza: TextView = view.findViewById(R.id.razaMascota)
+        val txtEdad: TextView = view.findViewById(R.id.edadMascota)
+        val txtNombre: TextView = view.findViewById(R.id.nombreMascota)
+        val txtSexo: TextView = view.findViewById(R.id.sexoMascota)
+        val imagen : ImageView = view.findViewById(R.id.imagenPerro)
+
+        val mascota: Mascota = args.Mascota
+
+
+        txtRaza.text = mascota.raza
+        txtEdad.text = mascota.edad.toString()
+        txtNombre.text = mascota.nombre
+        txtSexo.text = mascota.sexo
+        return view
     }
 }
