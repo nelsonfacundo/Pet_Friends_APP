1141cd97fa7d5d7e029776f6c226c1021069965c
maticeri1 <maticeri1@gmail.com>
2024-05-26 19:56:27 -0300

    boton back y adoptar.

--- app/src/main/java/com/example/petfriendsapp/fragments/AdoptarMascotaFragment.kt
+++ app/src/main/java/com/example/petfriendsapp/fragments/AdoptarMascotaFragment.kt
@@ -5,12 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import com.example.petfriendsapp.R
 
 
 class AdoptarMascotaFragment : Fragment() {
 
+    private lateinit var backButton: ImageView
+    private lateinit var buttonAdoptarMascota: Button
 
+    companion object {
+        val BACK_BUTTON_ID = R.id.ic_back_adoptar_mascota
+        val BUTTON_ADOPTAR_MASCOTA = R.id.button_adoptar_mascota
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -24,7 +32,5 @@ class AdoptarMascotaFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_adoptar_mascota, container, false)
     }
 
-    companion object {
 
-    }
 }
