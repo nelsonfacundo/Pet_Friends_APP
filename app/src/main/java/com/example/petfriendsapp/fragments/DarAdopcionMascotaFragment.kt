4d509d7d1add1ebf4ed429fc6c8bbc334b6f5771
maticeri1 <maticeri1@gmail.com>
2024-05-27 14:04:22 -0300

    avance en vista darAdopcionMascota y su fragment.

--- app/src/main/java/com/example/petfriendsapp/fragments/DarAdopcionMascotaFragment.kt
+++ app/src/main/java/com/example/petfriendsapp/fragments/DarAdopcionMascotaFragment.kt
@@ -1,11 +1,13 @@
 package com.example.petfriendsapp.fragments
 
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Im
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import com.example.petfriendsapp.R
 
@@ -15,6 +17,15 @@ class DarAdopcionMascotaFragment : Fragment() {
     private lateinit var backButton: ImageView
     private lateinit var buttonDarAdopcionMascota: Button
     private lateinit var viewDarAdopcionMascota: View
+    private lateinit var buttonSeleccionarFoto: Button
+    private lateinit var editTextEspecie: EditText
+    private lateinit var editTextRaza: EditText
+    private lateinit var editTextNombre: EditText
+    private lateinit var editTextEdad: EditText
+    private lateinit var editTextUbicacion: EditText
+    private lateinit var editTextSexo: EditText
+    private lateinit var editTextDescripcion: EditText
+    private lateinit var imageViewFotoMascota: ImageView
 
     companion object {
         val BACK_BUTTON_ID = R.id.ic_back_dar_adopcion_mascota
