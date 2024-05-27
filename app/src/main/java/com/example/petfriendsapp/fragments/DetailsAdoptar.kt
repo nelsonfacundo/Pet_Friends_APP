9ab93866fe2ebeadb9219beb470e9075669c617c
Matias Quesada <matiasquesada00@gmail.com>
2024-05-27 11:48:41 -0300

    nav volver al home del detalle

--- app/src/main/java/com/example/petfriendsapp/fragments/DetailsAdoptar.kt
+++ app/src/main/java/com/example/petfriendsapp/fragments/DetailsAdoptar.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.petfriendsapp.R
 import com.example.petfriendsapp.entities.Mascota
@@ -43,4 +45,5 @@ class DetailsAdoptar : Fragment() {
 
         return view
     }
+
 }
