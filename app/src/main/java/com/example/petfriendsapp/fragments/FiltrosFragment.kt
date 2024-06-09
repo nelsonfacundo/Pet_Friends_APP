e88d52759d3331c6d57080d1290ebe38d9e731d0
eileenss <eileenssanchez97@gmail.com>
2024-06-09 19:00:02 -0300

    filtros back, faltan mejoras

--- app/src/main/java/com/example/petfriendsapp/fragments/FiltrosFragment.kt
+++ app/src/main/java/com/example/petfriendsapp/fragments/FiltrosFragment.kt
@@ -7,10 +7,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.appcompat.app.AppCompatDialogFragment
+import androidx.core.os.bundleOf
 import com.example.petfriendsapp.R
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 
 class FiltrosFragment : BottomSheetDialogFragment() {
+
+    private lateinit var rangoEdad1a3: Chip
+    private lateinit var rangoEdad3a6: Chip
+    private lateinit var rangoEdad6a9: Chip
+    private lateinit var rangoEdadMas9: Chip
+    private var edadMin: Int = 0
+    private var edadMax: Int = Int.MAX_VALUE
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,12 +33,67 @@ class FiltrosFragment : BottomSheetDialogFragment() {
         val view = inflater.inflate(R.layout.fragment_filtros, container, false)
 
         val aplicarFiltros: Button = view.findViewById(R.id.btn_aplicar_filtros)
+        val limpiarFiltros: Button = view.findViewById(R.id.btn_limpiar_filtros)
+        rangoEdad1a3  = view.findViewById(R.id.chip_edad_1_3)
+        rangoEdad3a6 = view.findViewById(R.id.chip_edad_3_6)
+        rangoEdad6a9 = view.findViewById(R.id.chip_edad_6_9)
+        rangoEdadMas9 = view.findViewById(R.id.chip_edad_mas_9)
 
-        aplicarFiltros.setOnClickListener{
+        aplicarFiltros.setOnClickListener {
+            //Capturo filtros seleccionados
+            val especieSeleccionada = view.findViewById<ChipGroup>(R.id.chips_especie)
+                .checkedChipId.let {id ->view.findViewById<Chip>(id)?.text?.toString()}
 
+            val sexoSeleccionado = view.findViewById<ChipGroup>(R.id.chips_sexo)
+                .checkedChipId.let {id ->view.findViewById<Chip>(id)?.text?.toString()}
+
+            //Capturo rango de edad seleccionado
+            setRangoEdad()
+
+            //Guardo los filtros
+            val result = Bundle().apply {
+                putString("especieSeleccionada", especieSeleccionada)
+                putString("sexoSeleccionado", sexoSeleccionado)
+                putInt("edadMin", edadMin)
+                putInt("edadMax", edadMax)
+            }
+
+            //Paso de los filtros al inicio
+            parentFragmentManager.setFragmentResult("requestKey", result)
+
+            //Cierro bottomSheet
+            dismiss()
+        }
+
+        limpiarFiltros.setOnClickListener{
+            val result = Bundle().apply {
+                putString("especieSeleccionada", null)
+                putString("sexoSeleccionado", null)
+                putInt("edadMin", 0)
+                putInt("edadMax", Int.MAX_VALUE)
+            }
+
+            parentFragmentManager.setFragmentResult("requestKey", result)
+            dismiss()
         }
 
         return view
+    }
+
+    private fun setRangoEdad(){
+        if (rangoEdad1a3.isChecked){
+            edadMin = 1
+            edadMax = 3
+        } else if (rangoEdad3a6.isChecked){
+            edadMin = 3
+            edadMax = 6
+        } else if (rangoEdad6a9.isChecked){
+            edadMin = 6
+            edadMax = 9
+        } else if (rangoEdadMas9.isChecked){
+            edadMin = 9
+            edadMax = Int.MAX_VALUE
+        }
     }
 
     companion object {
--- app/src/main/java/com/example/petfriendsapp/fragments/Inicio.kt
+++ app/src/main/java/com/example/petfriendsapp/fragments/Inicio.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
 
 import android.widget.Button
+import androidx.fragment.app.setFragmentResultListener
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
 
@@ -22,21 +23,25 @@ import com.example.petfriendsapp.fragments.FiltrosFragment
 import com.example.petfriendsapp.viewmodels.ListViewModel
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 import com.google.firebase.Firebase
+import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import com.google.firebase.firestore.firestore
 
 class Inicio : Fragment() {
 
-
     private lateinit var viewInicio: View
     private lateinit var buttonCambiarPerfil: Button
     lateinit var recMascotas: RecyclerView
-    private lateinit var buttonDarEnAdopcion : Button
+    private lateinit var buttonDarEnAdopcion: Button
     private lateinit var viewModel: ListViewModel
     val db = Firebase.firestore
     private lateinit var btnFiltros: Button
-
     private lateinit var mascotaClickListener: (Mascota, String) -> Unit
+    private var especieSeleccionada: String? = null
+    private var sexoSeleccionado: String? = null
+    private var edadMin: Int = 0
+    private var edadMax: Int = Int.MAX_VALUE
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
@@ -45,6 +50,7 @@ class Inicio : Fragment() {
 
         checkRecords()
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -53,7 +59,7 @@ class Inicio : Fragment() {
 
         viewInicio = inflater.inflate(R.layout.fragment_inicio, container, false)
 
-        buttonDarEnAdopcion=viewInicio.findViewById(R.id.button_dar_en_adopcion)
+        buttonDarEnAdopcion = viewInicio.findViewById(R.id.button_dar_en_adopcion)
 
         recMascotas = viewInicio.findViewById(R.id.rec_mascota)
 
@@ -62,10 +68,9 @@ class Inicio : Fragment() {
             redirigir(mascota, mascotaId)
         }
 
+        //Abro bottomSheet de filtros
         btnFiltros = viewInicio.findViewById(R.id.btn_filtros)
-        btnFiltros.setOnClickListener{
-            showFiltrosBottomSheet()
-        }
+        btnFiltros.setOnClickListener { showFiltrosBottomSheet() }
 
         return viewInicio
 
@@ -74,18 +79,23 @@ class Inicio : Fragment() {
     private fun prepareFragment() {
         recMascotas.setHasFixedSize(true)
         recMascotas.layoutManager = LinearLayoutManager(context)
-
-
     }
-
 
     override fun onStart() {
         super.onStart()
 
+        //Recibo filtros de FiltrosFragment y actualizo el recycler
+        setFragmentResultListener("requestKey") { _, bundle ->
+            especieSeleccionada = bundle.getString("especieSeleccionada")
+            sexoSeleccionado = bundle.getString("sexoSeleccionado")
+            edadMin = bundle.getInt("edadMin")
+            edadMax = bundle.getInt("edadMax")
+            fillRecycler()
+        }
+
         fillRecycler()
 
         initListeners()
-
 
 
     }
@@ -94,39 +104,52 @@ class Inicio : Fragment() {
 
         val rootRef = FirebaseFirestore.getInstance()
 
-        val query = rootRef.collection("mascotas")
+        var query: Query = rootRef.collection("mascotas")
+
+        especieSeleccionada?.let {
+            if (!it.isNullOrEmpty()) query = query.whereEqualTo("especie", it)
+        }
+
+        sexoSeleccionado?.let {
+            if (!it.isNullOrEmpty()) query = query.whereEqualTo("sexo", it)
+        }
+
+        query = query.whereGreaterThanOrEqualTo("edad", edadMin)
+            .whereLessThanOrEqualTo("edad", edadMax)
 
         val options = FirestoreRecyclerOptions.Builder<Mascota>()
             .setQuery(query, Mascota::class.java)
             .build()
 
-        val adapter = MascotaFirestoreRecyclerAdapter(options,mascotaClickListener)
+        val adapter = MascotaFirestoreRecyclerAdapter(options, mascotaClickListener)
         adapter.startListening()
         recMascotas.adapter = adapter
     }
 
     private fun redirigir(mascota: Mascota, mascotaId: String) {
-        val action = InicioDirections.actionInicioToDetailsAdoptar(mascota, mascotaId) // Pasar el id a DetailsAdoptar
+        val action = InicioDirections.actionInicioToDetailsAdoptar(
+            mascota,
+            mascotaId
+        ) // Pasar el id a DetailsAdoptar
         findNavController().navigate(action)
     }
-
 
 
     private fun checkRecords() {
         db.collection("mascotas").get()
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                        for (mascota in viewModel.mascotas) {
-                            db.collection("mascotas").document().get()
+                    for (mascota in viewModel.mascotas) {
+                        db.collection("mascotas").document().get()
 
-                        }
                     }
+                }
 
             }
     }
 
-    private fun initListeners(){
-        buttonDarEnAdopcion.setOnClickListener{navigateToDarAdopcionMascota()}
+    private fun initListeners() {
+        buttonDarEnAdopcion.setOnClickListener { navigateToDarAdopcionMascota() }
     }
 
     private fun navigateToDarAdopcionMascota() {
@@ -134,7 +157,7 @@ class Inicio : Fragment() {
         viewInicio.findNavController().navigate(action)
     }
 
-    private fun showFiltrosBottomSheet(){
+    private fun showFiltrosBottomSheet() {
         val filtrosBottomSheet = FiltrosFragment()
         filtrosBottomSheet.show(parentFragmentManager, "FiltrosFragment")
     }
