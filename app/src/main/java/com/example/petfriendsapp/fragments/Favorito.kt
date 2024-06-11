7df0a0e3af74d39a5d6237290fbbc387dff32371
Ragueb <matias_ketlun@yahoo.com.ar>
2024-06-10 23:42:23 -0300

    Favorito

--- app/src/main/java/com/example/petfriendsapp/fragments/Favorito.kt
+++ app/src/main/java/com/example/petfriendsapp/fragments/Favorito.kt
@@ -5,19 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.RecyclerView
 import com.example.petfriendsapp.R
+import com.example.petfriendsapp.entities.Mascota
+import com.example.petfriendsapp.viewmodels.ListViewModel
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 
 class Favorito : Fragment() {
 
     private lateinit var viewFavorito: View
-    private lateinit var buttonBack: ImageView
+    private lateinit var buttonPerfilMascota: Button
+    lateinit var recMascotas: RecyclerView
+    private lateinit var viewModel: ListViewModel
+    val db = Firebase.firestore
 
-
+    private lateinit var mascotaClickListener: (Mascota, String) -> Unit
     companion object {
-        val BUTTON_BACK = R.id.ic_back_favorito
+        val BUTTON_BACK = R.id.ic_back_favorite
     }
 
 
@@ -27,6 +36,8 @@ class Favorito : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         viewFavorito=inflater.inflate(R.layout.fragment_favorito, container, false)
+
+
        initViews()
         return viewFavorito
 
@@ -39,17 +50,17 @@ class Favorito : Fragment() {
 
     }
     private fun initViews() {
-        buttonBack = viewFavorito.findViewById(BUTTON_BACK)
+        buttonPerfilMascota = viewFavorito.findViewById(R.id.card_mascota)
     }
 
 
     private fun initListeners() {
-        buttonBack.setOnClickListener { navigateToHome() }
+        BUTTON_BACK.setOnClickListener { navigateToHome() }
 
     }
     private fun navigateToHome() {
-        val action1 = ReviewFragmentDirections.actionReviewFragmentToInicio()
-        viewFavorito.findNavController().navigate(action1)
+      //  val action1 = ReviewFragmentDirections.actionReviewFragmentToInicio()
+      //  viewFavorito.findNavController().navigate(action1)
     }
 
 
