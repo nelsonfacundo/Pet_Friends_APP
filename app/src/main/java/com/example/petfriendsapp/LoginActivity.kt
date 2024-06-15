4ea5c20fbddec8e7583f00a7d934de49fa5e4d7d
Facundo nelson <facundo.nelson@fluxit.com.ar>
2024-06-15 02:34:17 -0300

    rename

--- app/src/main/java/com/example/petfriendsapp/LoginActivity.kt
+++ app/src/main/java/com/example/petfriendsapp/LoginActivity.kt
@@ -100,7 +100,7 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun navigateToRegister() {
-        val intent = Intent(this, RegistroActivity::class.java)
+        val intent = Intent(this, RegisterActivity::class.java)
         startActivity(intent)
         finish()
     }
